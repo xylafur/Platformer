ce12a82e335653f09fbcd51c7e1f3007d4034660
Kesley Richardson <jkesleyrichardson@gmail.com>
2016-09-25 16:59:20 -0500

    Added better functionality and new aspect.  Also updated the html

--- platform.js
+++ platform.js
@@ -1,50 +1,67 @@
-
 //creates the canvas
-var won = false;
-
 var canvas = document.createElement("canvas"), c = canvas.getContext("2d");
 canvas.width = 800; canvas.height = 400;document.body.appendChild(canvas);
-var gravity = - 2;
+var gravity = - 5;
+var won = false;
 
 //This is just an array that holds all of the platform objects
 var platforms =  [new Platform(0, canvas.height - 60), new Platform(30, canvas.height - 30) , new Platform(100, canvas.height - 100), new Platform(20, canvas.height - 140), new Platform(canvas.width - 180, canvas.height - 150), new Platform(canvas.width - 220, 210, 10, 20), new Platform(canvas.width - 300, 190), new Platform(canvas.width - 350, 190, 10, 20), new Platform(canvas.width - 400, 190, 10, 20), new Platform(canvas.width - 500, 150)];
-
-var platLength = 50; //width of platforms
-
-
+//width of platforms
+var platLength = 50;
+//player object
 var p = new Player();
+//the box that you have to get to to win
 var win = new WinBox(canvas.width - 500, 140);
+
+var gravityTimer = 300;
+var gravityShifted = false;
 
 //draws the background, platforms and the player
 setInterval(function(){
   checkWin();
- c.fillStyle = "green";
-  c.fillRect(0, 3 * canvas.height / 4, canvas.width, canvas.height);
-  c.fillStyle = "blue";
-  c.fillRect(0, 0, canvas.width, 3 * canvas.height /4);
-  p.draw();
-
-  //alert(2);
-
-  for(i = 0; i < platforms.length; i++){
-    platforms[i].draw();
-    //alert(platforms[i].x);
-  }
-  win.draw();
+  setBoard();
+  changeGravity();
 },30);
 
-
+//calls everythings draw function
+function setBoard(){
+  c.fillStyle = "green";
+   c.fillRect(0, 3 * canvas.height / 4, canvas.width, canvas.height);
+   c.fillStyle = "blue";
+   c.fillRect(0, 0, canvas.width, 3 * canvas.height /4);
+   p.draw();
+   for(i = 0; i < platforms.length; i++){
+     platforms[i].draw();
+     //alert(platforms[i].x);
+   }
+   win.draw();
+};
+//wil change gravty
+function changeGravity(){
+  if(gravityShifted && gravityTimer >0){
+    gravity= -2;}
+  else{
+    gravityShifted = false;
+    gravity = -5;
+    gravityTimer = 30;}
+    gravityTimer--;
+};
 //checks for user input to move player if possible
 document.addEventListener('keydown', function(event){
   if(event.keyCode == 37){
-    p.vx = -p.sideLength;
-  }else if(event.keyCode == 39){
-    p.vx = p.sideLength;
-  }
-  //if the player is on the ground and the user
-  //hits space then the jump method will trigger.
+    p.vx = -p.sideLength;}
+  else if(event.keyCode == 39){
+    p.vx = p.sideLength;}
+  if(event.keyCode == 71 && gravityTimer > 0)
+    gravityShifted = true;
   if(event.keyCode == 32 && p.grounded){
-    p.jumping = true;
+    p.jumping = true;}
+});
+//sets horizontal component back to zero when the user
+//lets go of the key
+document.addEventListener('keyup', function(event){
+  if(event.keyCode == 37 || event.keyCode == 39){
+    p.vx = 0;
   }
 });
 
@@ -60,23 +77,23 @@ Platform.prototype.draw = function(){
   c.fillRect(this.x, this.y, this.width, this.height);
 }
 
-//Box that the player must reach to win
-function WinBox(xPos, yPos){
-  this.x = xPos;this.y = yPos;
-}
-WinBox.prototype.draw = function(){
-  c.fillStyle ="purple";
-  c.fillRect(this.x, this.y, p.sideLength, p.sideLength);
-}
-
-//checks to see if the player intersects the win box and wins the game!
-checkWin = function(){
-  if(p.x == win.x && p.y ==win.y){
-
-      alert("You win!");
-
+  //Box that the player must reach to win
+  function WinBox(xPos, yPos){
+    this.x = xPos;this.y = yPos;
   }
-}
+  WinBox.prototype.draw = function(){
+    c.fillStyle ="purple";
+    c.fillRect(this.x, this.y, p.sideLength, p.sideLength);
+  }
+
+  //checks to see if the player intersects the win box and wins the game!
+  checkWin = function(){
+    if(p.x == win.x && p.y ==win.y){
+        alert("You win!");
+        p.x = canvas.width / 2;
+        p.y = canvas.height;
+      }
+    }
 
 //All below relates to the player object
 function Player(){
